708f547288b69acfd7e914bc9119fb2e84c08c0b
wlswo <byunjin11@naver.com>
2023-07-05 09:15:33 +0900

    Initial commit

---
+++ src/main/java/com/oidev/openstack4j/Openstack4jApplication.java
@@ -0,0 +1,13 @@
+package com.oidev.openstack4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openstack4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openstack4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oidev/openstack4j/Openstack4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oidev.openstack4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Openstack4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
